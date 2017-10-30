c435c134600f983074d29ddfabad98a456e7f214
jkafrouni <j.kafrouni@gmail.com>
2017-10-30 18:04:11 -0400

    quick fix to avoid empty sentences after preprocessing

--- iterative_set_expansion/preprocess.py
+++ iterative_set_expansion/preprocess.py
@@ -5,4 +5,7 @@ def split_sentences(text):
     Given a paragraph in one single string, 
     splits each sentence and returns a list of strings, one string for each sentence.
     """
-    return nltk.tokenize.sent_tokenize(text)
+    sentences = nltk.tokenize.sent_tokenize(text)
+    while '' in sentences: # quick fix, nltk might add empty sentences
+        sentences.remove('')
+    return sentences
