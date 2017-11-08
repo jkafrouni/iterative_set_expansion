276a9c2d5eb4b015e59d6541d3b786cde7e7dabf
jkafrouni <j.kafrouni@gmail.com>
2017-11-07 19:56:45 -0500

    changed urllib parser to avoid problems on VM + catching Exceptions better in scraper's threadpool

--- iterative_set_expansion/scrape.py
+++ iterative_set_expansion/scrape.py
@@ -18,27 +18,26 @@ def scrape(url):
     logger.debug('[SCRAPER]\t Loading url: %s', url)
     try:
         html_page = urlopen(url).read()
-    except (http.client.IncompleteRead, urllib.error.URLError):
+    except Exception as e:
+        logger.warning("[SCRAPER]\t Could not load the page for url: %s", url)
+        logger.error(e)
+        return ''
+
+    logger.debug('[SCRAPER]\t Parsing with BS')
+    try:
+        soup = BeautifulSoup(html_page, 'html.parser')
+        data = soup.findAll('p')
+        data = [p.get_text().replace('\n', '').replace('\t','') for p in data]
+    except Exception as e:
         logger.warning("[SCRAPER]\t Could not read the page for url: %s", url)
-        return ''
-    except Exception:
-        logger.warning("[SCRAPER]\t Unidentified error for url: %s", url)
-        return ''
+        logger.error(e)
+
+    if not data:
+        logger.warning('[SCRAPER]\t No data found for url: %s', url)
     else:
-        logger.debug('[SCRAPER]\t Parsing with BS')
-        try:
-            soup = BeautifulSoup(html_page, 'html5lib')
-            data = soup.findAll('p')
-            data = [p.get_text().replace('\n', '').replace('\t','') for p in data]
-        except Exception as e:
-            print('Exception with BS: ', e)
+        logger.debug('[SCRAPER]\t [%s]: \n %s', url, data)
 
-        if not data:
-            logger.warning('[SCRAPER]\t No data found for url: %s', url)
-        else:
-            logger.debug('[SCRAPER]\t [%s]: \n %s', url, data)
-
-        return ' '.join(data) if data else ''
+    return ' '.join(data) if data else ''
 
 
 def add_url_content(query, documents):
